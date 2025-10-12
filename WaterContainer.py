e6b9f47c7d668aca4e3cf7a7574b3f1004cce0c4
Abhilash Palit <abhilashpalit80@gmail.com>
2025-10-12 11:40:10 +0530

    12/10/2025 I

---
+++ WaterContainer.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        
+        res=[]
+
+        left,right=0,len(height)-1
+        
+        while left<right:
+            res.append( min(height[left],height[right]) * (right-left))
+            if(height[left]>height[right]):
+               right-=1
+
+            elif(height[right]>height[left]):
+                left+=1
+            
+            else:
+                left+=1
+                right-=1
+        
+        return max(res)
+        
