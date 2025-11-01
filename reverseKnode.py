a4b2e4eda6cbcfb5cf073368b5770037ddf95a46
Abhilash Palit <abhilashpalit80@gmail.com>
2025-11-01 23:29:12 +0530

    01/11/2025 I

--- reverseKnode.py
+++ reverseKnode.py
@@ -1,12 +1,11 @@
-import sys
-
-def kinkedList():
-    class ListNode:
-        def __init__(self, val=0, next=None):
-            self.val = val
-            self.next = next
-
-    def KGroup(head: ListNode, k: int) -> ListNode:
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        
         curr = head
         count = 0
         while curr and count < k:
@@ -25,38 +24,38 @@ def kinkedList():
             prev = current
             current = next_node
             
-        head.next = KGroup(current, k)
+        head.next = self.reverseKGroup(current, k)
         
         return prev
 
-    try:
-        line1 = sys.stdin.readline().strip().split()
-        if not line1: return
-        
-        k = int(line1[1])
-        
-        elements_ = sys.stdin.readline().strip().split()
-        elements = list(map(int, elements_))
-        
-    except Exception:
-        return
+        try:
+            line1 = sys.stdin.readline().strip().split()
+            if not line1: return
+            
+            k = int(line1[1])
+            
+            elements_ = sys.stdin.readline().strip().split()
+            elements = list(map(int, elements_))
+            
+        except Exception:
+            return
 
-    dummy = ListNode(0)
-    current_node = dummy
-    for val in elements:
-        current_node.next = ListNode(val)
-        current_node = current_node.next
-    
-    head = dummy.next
-    
-    new_head = KGroup(head, k)
-    
-    output = []
-    current = new_head
-    while current:
-        output.append(str(current.val))
-        current = current.next
+        dummy = ListNode(0)
+        current_node = dummy
+        for val in elements:
+            current_node.next = ListNode(val)
+            current_node = current_node.next
         
-    print(" ".join(output))
+        head = dummy.next
+        
+        new_head = self.reverseKGroup(head, k)
+        
+        output = []
+        current = new_head
+        while current:
+            output.append(str(current.val))
+            current = current.next
+            
+        print(" ".join(output))
 
-kinkedList()
+
