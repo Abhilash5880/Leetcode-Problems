f65d7bc6c3366679dfd7a773099b53a90fa40e85
Abhilash Palit <abhilashpalit80@gmail.com>
2026-05-17 23:41:59 +0530

    17-05-2025 I

---
+++ LargestHistogram.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+
+    int largestRectangleArea(vector<int>& heights)
+    {
+        stack<int> st;
+
+        int maxArea = 0;
+        int n = heights.size();
+
+        for(int i = 0; i < n; i++)
+        {
+            while(!st.empty() &&
+                  heights[i] < heights[st.top()])
+            {
+                int height = heights[st.top()];
+                st.pop();
+
+                int right = i;
+
+                int left;
+
+                if(st.empty())
+                    left = -1;
+                else
+                    left = st.top();
+
+                int width = right - left - 1;
+
+                maxArea =
+                    max(maxArea, height * width);
+            }
+
+            st.push(i);
+        }
+
+        while(!st.empty())
+        {
+            int height = heights[st.top()];
+            st.pop();
+
+            int right = n;
+
+            int left;
+
+            if(st.empty())
+                left = -1;
+            else
+                left = st.top();
+
+            int width = right - left - 1;
+
+            maxArea =
+                max(maxArea, height * width);
+        }
+
+        return maxArea;
+    }
+};
