7192884bcaeb8b691e88ccea9bf811bad150f1e7
Abhilash Palit <abhilashpalit80@gmail.com>
2026-05-29 21:07:41 +0530

    29-05-25

---
+++ lastStoneWeight.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) 
+    {
+        priority_queue<int, vector<int>, less<int>> maxHeap;
+
+        for (int stone : stones)
+        {
+            maxHeap.push(stone);
+        }
+
+        while(maxHeap.size() > 1)
+        {
+            int y = maxHeap.top();
+            maxHeap.pop();
+
+            int x = maxHeap.top();
+            maxHeap.pop();
+
+            if(y != x)
+            {
+                maxHeap.push(y - x);
+            }
+            if(y==x) continue;
+        }
+        return maxHeap.empty() ? 0 : maxHeap.top();
+    }
+};
